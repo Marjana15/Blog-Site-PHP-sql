3346646b10dbee2fcda153e82fcc5cedeac77ae2
Marjana Begum <121622736+Marjana15@users.noreply.github.com>
2024-12-25 14:54:32 +0600

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,9 +1,6 @@
 <?php
 include 'config/constants.php';
 
-$username_email = $_SESSION['signin-data']['username_email'] ?? null;
-$password = $_SESSION['signin-data']['password'] ?? null;
-
 unset($_SESSION['signin-data']);
 ?>
 <!DOCTYPE html>
@@ -15,6 +12,7 @@ unset($_SESSION['signin-data']);
     <title>Content Connect</title>
     <!-- CUSTOM STYLESHEET -->
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="https://t3.ftcdn.net/jpg/03/47/53/38/360_F_347533897_K39mGJqveEng84SEgJpBbOQWsavLbXAm.jpg" type="image/x-icon">
     <!-- ICONSCOUT CDN -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <!-- GOOGLE FONT(MONTSERATE) -->
@@ -44,12 +42,22 @@ unset($_SESSION['signin-data']);
             </div>
         <?php endif; ?>
         <form action="<?= ROOT_URL ?>signin-logic.php" method="POST">
-            <input type="text" name="username_email" value='<?= $username_email ?>' placeholder="Username or Email">
-            <input type="password" name="password" value='<?= $password ?>' placeholder=" Password">
+            <input id="username_email" type="text" name="username_email" placeholder="Username or Email">
+            <input id="password" type="password" name="password" placeholder="Password">
             <button type="submit" class="btn" name="submit">Sign in</button>
             <small>Don't have an account? <a href="signup.php">Sign up</a></small>
         </form>
+        <!-- Prefill Button -->
+        <button class="btn prefill-btn" onclick="prefillDemo()">Use Demo Login</button>
     </div>
 </section>
+
+<script>
+    function prefillDemo() {
+        // Set the input values for demo login
+        document.getElementById('username_email').value = 'marju';
+        document.getElementById('password').value = '12345678';
+    }
+</script>
 </body>
 </html>
